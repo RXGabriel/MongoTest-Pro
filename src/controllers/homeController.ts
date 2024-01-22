53a5cc251071ae064f9a440de706bf35e8678021
RXGabriel <gabriel2005rf@gmail.com>
2024-01-22 15:17:15 -0300

    Updating data in MongoDB

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,6 +2,16 @@ import { Request, Response } from 'express';
 import UserModel from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
+  await UserModel.updateOne(
+    {email: 'giornoGiovanna@gmail.com'},
+    {age:15 }
+  );
+
+  let user =await UserModel.findByIdAndUpdate(
+    
+  );
+
+
    let users = await UserModel.find({}).sort({"name.firstName": 1});
 
    res.render('pages/home', {

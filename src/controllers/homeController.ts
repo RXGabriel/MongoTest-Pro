bc276e2a9bbe1a791d436242b912f8b584889ebb
RXGabriel <gabriel2005rf@gmail.com>
2024-01-22 15:29:34 -0300

    Deleting data in MongoDB

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,15 +2,9 @@ import { Request, Response } from 'express';
 import UserModel from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
-  await UserModel.updateOne(
-    {email: 'giornoGiovanna@gmail.com'},
-    {age:15 }
-  );
-
-  let user =await UserModel.findByIdAndUpdate(
-    
-  );
-
+  await UserModel.findByIdAndDelete(
+    {email: 'suporteGabriel@gmail.com'}
+  )
 
    let users = await UserModel.find({}).sort({"name.firstName": 1});
 

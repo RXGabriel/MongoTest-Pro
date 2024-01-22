26629c7c03c4aa962eaa8839a24d5bbf739efad2
RXGabriel <gabriel2005rf@gmail.com>
2024-01-22 11:09:41 -0300

    Inserting data into MongoDB

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,11 +3,28 @@ import { Product } from '../models/Product';
 import UserModel from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
-  let users = await UserModel.find({
-    age: { $gt: 18 }
-  }).sort({ "name.firstName": 1, "name.lastName": 1 }).skip(0)
-  .limit(2);
-  console.log("Users", users);
+    let newUser = new UserModel()
+    newUser.name = {
+        firstName: 'André',
+        lastName: 'Lacerda'
+    }
+    newUser.email = 'andre@Lc.org'
+    newUser.age = 45
+    newUser.interests = ['programação', 'anime']
+    let resultado = await newUser.save()
+    console.log("NEW USER", resultado)
+
+
+    let newUserr = await UserModel.create({
+        name: {
+            firstName: 'Monaliza',
+            lastName: 'Lacerda'
+        },
+        email: 'mona@paris.org',
+        age: 90,
+        interests: ['arte','musica']
+    })
+    console.log("NEW USER", newUserr);
 
   let age: number = 90;
   let showOld: boolean = false;
@@ -23,7 +40,6 @@ export const home = async (req: Request, res: Response) => {
     name: 'Bonieky',
     lastName: 'Lacerda',
     showOld,
-    users,
     products: list,
     expensives: expensiveList,
     frasesDoDia: []
